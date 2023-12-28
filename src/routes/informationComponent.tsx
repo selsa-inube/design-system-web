4fefb48026e9989f46f0bea5fc019bf2478eadcb
Andres Babativa <carlos3k11@gmail.com>
2023-12-28 16:54:12 -0500

    feat(CountdownBar): add CountdownBar component and related files

--- src/routes/informationComponent.tsx
+++ src/routes/informationComponent.tsx
@@ -14,6 +14,7 @@ import { PageTable } from "@pages/components/data/Table";
 import { PageTag } from "@pages/components/data/Tag";
 import { PageUser } from "@pages/components/data/User";
 import { PageAssisted } from "@pages/components/feedback/Assisted";
+import { PageCountdownBar } from "@pages/components/feedback/CountdownBar";
 
 export const InformationComponentRoutes = () => {
   return (
@@ -25,6 +26,7 @@ export const InformationComponentRoutes = () => {
       <Route path="table" element={<PageTable />} />
       <Route path="user" element={<PageUser />} />
       <Route path="assisted" element={<PageAssisted />} />
+      <Route path="countdown-bar" element={<PageCountdownBar />} />
       <Route path="button" element={<PageButton />} />
       <Route path="label" element={<PageLabel />} />
       <Route path="select" element={<PageSelect />} />
