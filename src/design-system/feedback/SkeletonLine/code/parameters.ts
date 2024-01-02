bdcbd19c7b85c7af96c8dc9c2475676f0b081680
Andres Babativa <carlos3k11@gmail.com>
2024-01-02 09:50:46 -0500

    feat(SkeletonLine): add SkeletonLine component and its code

---
+++ src/design-system/feedback/SkeletonLine/code/parameters.ts
@@ -0,0 +1,14 @@
+import { InformationsProps } from "@components/data/InformationProps";
+
+export const parameters: InformationsProps[] = [
+  {
+    nameProps: "width",
+    description: "Indicates the width that the component should take",
+    type: "string",
+  },
+  {
+    nameProps: "animated",
+    description: "Enable loading effect animation",
+    type: "boolean",
+  },
+];
