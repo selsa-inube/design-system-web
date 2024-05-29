c11cbde24de40dc706f6c5d118622c209443a5ab
cmarin001 <cmarin6051@gmail.com>
2024-05-29 13:41:17 -0500

    fix(divider): include divider section

--- src/content/layout/Divider/index.ts
+++ src/content/layout/Divider/index.ts
@@ -1,4 +1,19 @@
+import { buildTokenDescriptions } from "../../tokens/buildTokenDescriptions";
 import { DividerController } from "./Controller/Divider.Controller";
+import { inube } from "@inubekit/foundations";
+
+const dividerTokensConfig = {
+  businessUnit: "inube",
+  component: "divider",
+  include: [
+    { id: "businessUnit", order: 1 },
+    { id: "component", order: 2 },
+    { id: "block", order: 3 },
+    { id: "element", order: 4 },
+    { id: "token", order: 5 },
+    { id: "reference", order: 6 },
+  ],
+};
 
 const divider = {
   description:
@@ -12,8 +27,11 @@ const divider = {
     dashed: {
       description:
         "It determines if the divider will be dashed or not. If not provided, the divider will be solid.",
+      type: "boolean",
+      default: false,
     },
   },
+  tokens: buildTokenDescriptions(inube.divider, dividerTokensConfig),
 };
 
 export { divider };
